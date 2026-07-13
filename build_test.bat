46146ce9350fba872eda87dbad7405e7c49a80b7
GBwater <gbwater@outlook.com>
2026-07-14 03:13:48 +0800

    build: align debug runtimes and silence CMake dev warning

--- build_test.bat
+++ build_test.bat
@@ -1,7 +1,7 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat" >nul
 cd /d D:\md-cpp
-cmake -G Ninja -B build
+cmake -Wno-dev -G Ninja -B build
 if errorlevel 1 exit /b %errorlevel%
 cmake --build build --target elmd_tests
 if errorlevel 1 exit /b %errorlevel%
